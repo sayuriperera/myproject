89a2827302a8be8da473c8b44485e541240f4212
PERERA W.S.A. WILARACHCHIGE SAYURI ANUTHTHARA PERERA <sayu.anuththara@gmail.com>
2020-05-23 21:29:34 +0530

    trying commit

--- library/CreateProfile.aspx.cs
+++ library/CreateProfile.aspx.cs
@@ -8,12 +8,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
 namespace library
 {
     public partial class CreateProfile : System.Web.UI.Page
     {
-        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        private readonly string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -22,49 +21,89 @@ namespace library
         //Create Employee Button
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //Response.Write("<script> alert('test');</script>");
-            try
+            if (checkIDExists())
             {
-                string filepath = "~/Employee_photos/social-media.png";
-                string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
-                FileUpload1.SaveAs(Server.MapPath("Employee_photos/" + filename));
-                filepath = "~/Employee_photos/" + filename;
 
-
-                SqlConnection con = new SqlConnection(strcon);
-                if(con.State == ConnectionState.Closed)
-                {
-                    con.Open();
-                    //Response.Write("<script> alert('hii');</script>");
-                }
-                
-
-                SqlCommand cmd = new SqlCommand("INSERT INTO Emp_Detail(Emp_ID,First_Name,Last_Name,Full_Name,NIC,DOB,Gender,Address,City,District,Contact_No,Joined_Date,Status,Post,Department,Image,Qualifications) values(@Emp_ID,@First_Name,@Last_Name,@Full_Name,@NIC,DOB,@Gender,@Address,@City,@District,@Contact_No,@Joined_Date,@Status,@Post,@Department,@Image,@Qualifications)", con);
-                cmd.Parameters.AddWithValue("@Emp_ID", TextBox11.Text.Trim());
-                cmd.Parameters.AddWithValue("@First_Name", TextBox1.Text.Trim());
-                cmd.Parameters.AddWithValue("@Last_Name", TextBox3.Text.Trim());
-                cmd.Parameters.AddWithValue("@Full_Name", TextBox4.Text.Trim());
-                cmd.Parameters.AddWithValue("@NIC", TextBox2.Text.Trim());
-                cmd.Parameters.AddWithValue("@DOB", TextBox5.Text.Trim());
-                cmd.Parameters.AddWithValue("@Gender",DropDownList1.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@Address", TextBox6.Text.Trim());
-                cmd.Parameters.AddWithValue("@City", TextBox7.Text.Trim());
-                cmd.Parameters.AddWithValue("@District",DropDownList2.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@Contact_No", TextBox8.Text.Trim());
-                cmd.Parameters.AddWithValue("@Joined_Date", TextBox9.Text.Trim());
-                cmd.Parameters.AddWithValue("@Status",DropDownList3.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@Post", TextBox10.Text.Trim());
-                cmd.Parameters.AddWithValue("@Department",DropDownList4.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@Image",filepath);
-                cmd.Parameters.AddWithValue("@Qualifications", TextBox12.Text.Trim());
-
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("<script> alert('Member created successfully.');</script>");
+                Response.Write("<script>alert('Member Already Exist with this Member ID, try other ID');</script>");
             }
-            catch(Exception ex)
+            //Response.Write("<script> alert('test');</script>");
+            else
             {
-                Response.Write("<script> alert('" + ex.Message +"');</script>");
+                createMember();
+            }
+
+            bool checkIDExists()
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    SqlCommand cmd = new SqlCommand("SELECT * from mEmp_Detail where Emp_ID='" + TextBox11.Text.Trim() + "';", con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count >= 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                    return false;
+                }
+            }
+
+            void createMember()
+            {
+                try
+                {
+                    //string filepath = "~/Employee_photos/social-media.png";
+                    //string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    //FileUpload1.SaveAs(Server.MapPath("Employee_photos/" + filename));
+                    //filepath = "~/Employee_photos/" + filename;
+
+
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                        //Response.Write("<script> alert('hii');</script>");
+                    }
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Emp_Detail(Emp_ID,First_Name,Last_Name,Full_Name,NIC,DOB,Gender,Address,City,District,Contact_No,Joined_Date,Status,Post,Department,Qualifications) values(@Emp_ID,@First_Name,@Last_Name,@Full_Name,@NIC,@DOB,@Gender,@Address,@City,@District,@Contact_No,@Joined_Date,@Status,@Post,@Department,@Qualifications)", con);
+                    cmd.Parameters.AddWithValue("@Emp_ID", TextBox11.Text.Trim());
+                    cmd.Parameters.AddWithValue("@First_Name", TextBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Last_Name", TextBox3.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Full_Name", TextBox4.Text.Trim());
+                    cmd.Parameters.AddWithValue("@NIC", TextBox2.Text.Trim());
+                    cmd.Parameters.AddWithValue("@DOB", TextBox5.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Gender", DropDownList1.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@Address", TextBox6.Text.Trim());
+                    cmd.Parameters.AddWithValue("@City", TextBox7.Text.Trim());
+                    cmd.Parameters.AddWithValue("@District", DropDownList2.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@Contact_No", TextBox8.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Joined_Date", TextBox9.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Status", DropDownList3.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@Post", TextBox10.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Department", DropDownList4.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@Qualifications", TextBox12.Text.Trim());
+                    //cmd.Parameters.AddWithValue("@Image", filepath);
+                    //_ = cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("<script> alert('Member created successfully.');</script>");
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script> alert('" + ex.Message + "');</script>");
+                }
             }
         }
     }
